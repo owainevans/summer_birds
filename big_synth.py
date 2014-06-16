58746c04f2f381e1398d4db73da93636d16ff242
Owain Evans <owain@mit.edu>
2014-06-16 15:50:44 -0400

    d

--- big_synth.py
+++ big_synth.py
@@ -1,12 +1,15 @@
 from synthetic import *
 
 def recon(steps,iterations):
-    return test_recon((steps,iterations), test_hypers=True,plot=False,use_mh_filter=True)
+    out = test_recon((steps,iterations), test_hypers=True,plot=False,use_mh_filter=True)
+    uni,params,locs,figs,mses = out
+    inf_hypers = get_hypers(uni[2].ripl,2)
+    print 'inf_hypers',inf_hypers
+    return out
     
+#test    
 recon(0,0)
-inf_hypers = get_hypers(uni[2].ripl,2)
-print 'inf_hypers',inf_hypers
 print 'passed test'
+
 uni,params,locs,figs,mses = recon(10,5)
-inf_hypers = get_hypers(uni[2].ripl,2)
-print 'inf_hypers',inf_hypers
+
